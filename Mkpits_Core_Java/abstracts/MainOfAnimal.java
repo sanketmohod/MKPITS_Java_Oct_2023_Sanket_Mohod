4d95c38cb2d28eb1398153007685d1b1fe2ce6ba
sanketmohod <mohodsanket45gmail.com>
2024-01-11 11:08:48 +0530

    abstract class added

---
+++ Mkpits_Core_Java/abstracts/MainOfAnimal.java
@@ -0,0 +1,15 @@
+package com.mkpits.abstracts;
+
+public class MainOfAnimal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Pig pObj = new Pig();
+		pObj.getAbstract();
+		pObj.sleep();
+		pObj.animalSound();
+		Tiger tObj = new Tiger();
+		tObj.animalSound();
+	}
+
+}
