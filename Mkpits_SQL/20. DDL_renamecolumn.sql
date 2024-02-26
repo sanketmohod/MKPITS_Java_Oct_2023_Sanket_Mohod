2100561d7fd21415209c702c825cfb01f440a316
sanketmohod <mohodsanket45gmail.com>
2024-02-26 17:40:24 +0530

    sql files

---
+++ Mkpits_SQL/20. DDL_renamecolumn.sql
@@ -0,0 +1,6 @@
+use customer;
+-- rename command 
+-- This is used to rename an object existing in the database.
+-- rename to column of table
+alter table student_data
+rename column mobile to contact;
